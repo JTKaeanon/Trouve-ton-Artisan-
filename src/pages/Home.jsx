70790a085f762ebdb1f3e94e937aa992f559afb4
Jessie Texier <jessitexier8@gmail.com>
2026-01-09 09:32:55 +0100

    correction utf 8 pour é

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import ArtisanCard from '../components/artisancard/ArtisanCard';
 import artisansData from '../data/artisans.json'; 
 
 const Home = () => {
+  // On récupère uniquement les artisans du "Top" pour l'accueil
   const topArtisans = artisansData.filter(artisan => artisan.top === true);
 
   return (
@@ -10,17 +11,14 @@ const Home = () => {
       
       {/* Section Instructions */}
       <div className="container py-4 text-center">
-        
-        {/* MODIFICATION ICI : Ajout de mb-4 pour l'espace sous le titre */}
-        <h2 className="fw-bold mb-4" style={{ color: '#00497C' }}>
+        <h2 className="fw-bold" style={{ color: '#00497C' }}>
           Comment trouver mon <span style={{ color: '#82B864' }}>artisan</span> ?
         </h2>
-        
-        <ul className="list-unstyled d-inline-block text-start" style={{ color: '#00497C' }}>
-          <li>Étape 1 : Choisir la catégorie d'artisan dans le menu</li>
-          <li>Étape 2 : Choisir un artisan</li>
-          <li>Étape 3 : Le contacter via le formulaire de contact</li>
-          <li>Étape 4 : Une réponse sera apportée sous <span style={{ color: '#CD2C2E', fontWeight: 'bold' }}>48h</span></li>
+        <ul className="list-unstyled d-inline-block text-start mt-3" style={{ color: '#00497C' }}>
+          <li>&Eacute;tape 1 : Choisir la cat&eacute;gorie d'artisan dans le menu</li>
+          <li>&Eacute;tape 2 : Choisir un artisan</li>
+          <li>&Eacute;tape 3 : Le contacter via le formulaire de contact</li>
+          <li>&Eacute;tape 4 : Une r&eacute;ponse sera apport&eacute;e sous <span style={{ color: '#CD2C2E', fontWeight: 'bold' }}>48h</span></li>
         </ul>
       </div>
 
